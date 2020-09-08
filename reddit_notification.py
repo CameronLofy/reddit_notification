63357128a3600887f0940006a66abd1472a33653
Cameron <cameronlofy@gmail.com>
2020-09-08 14:40:46 -0700

    added push notification function and tested in main reddit_notification.py

---
+++ reddit_notification.py
@@ -0,0 +1,3 @@
+from send_push import *
+
+send_push("Test")
---
+++ send_push.py
@@ -0,0 +1,15 @@
+import http.client
+import urllib
+from config import app_token, user_token
+
+
+
+def send_push(message):
+    conn = http.client.HTTPSConnection("api.pushover.net:443")
+    conn.request("POST", "/1/messages.json",
+        urllib.parse.urlencode({
+            "token": app_token,
+            "user": user_token,
+            "message": message
+        }), {"Content-type": "application/x-www-form-urlencoded"})
+    conn.getresponse()
