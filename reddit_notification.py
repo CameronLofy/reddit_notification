b2b818d20d955e877792f2865d84a3279c89a93a
Cameron <cameronlofy@gmail.com>
2020-10-01 11:50:32 -0700

    added url shortener to make messages shorter for sms notifications and save money from Twilio

--- reddit_notification.py
+++ reddit_notification.py
@@ -1,8 +1,10 @@
 from send_push import *
 from send_sms import send_sms
+from url_shortener import make_tiny
 from config import reddit_client_id, reddit_client_secret, reddit_password, reddit_username
 from config import subreddits
 import praw
+
 
 #send_push("Test")
 
@@ -24,13 +26,17 @@ for i in subreddits:
             urls.append("https://www.reddit.com" + post.permalink)
             break
         
+# Shorten URLS:
+for i in range(0, len(urls)):
+    urls[i] = make_tiny(urls[i])
+    
+
+        
 push_message = "Top posts from:"
 text_message = push_message
 for i in range(0,len(subreddits)):
     push_message = push_message + f"\n<a href={urls[i]}>{subreddits[i]}</a>"
     text_message = text_message + f"\n\n{subreddits[i]} : \n{urls[i]} "
 
-
-print(push_message)
-send_sms(text_message)
+#send_sms(text_message)
 send_push(push_message)
